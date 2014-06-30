c6c6d68ffe497fefb0b27dd29077e5c7eb652e1f
Colin Scott <m.colin.scott@gmail.com>
2014-06-30 07:56:39 -0400

    Added year validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,15 +3,14 @@ class Car < ActiveRecord::Base
 
   validates :car_manufacturer_id, presence: true
   validates :color, presence: true
-  validates :year, presence: true, if: :after_1920?
+  validates :year, presence: true
   validates :mileage, presence: true
+  validate :after_1920?
 
 
   def after_1920?
-    if self.year > 1920
-      true
-    else
-      false
+    if year.present? && year < 1920
+      errors.add(:year, "Must be 1920 or later")
     end
   end
 end
